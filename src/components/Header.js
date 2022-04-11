325f95f0f779447f5f5d08c21529dd8d03f77ab9
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-04-11 20:13:32 +0300

    added styled

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import {StyledHeader} from "./styles/Header.styled"
+
+export default function Header() {
+    return (
+    
+        <StyledHeader bg="lightblue">
+            <h1>Hubble </h1>
+        </StyledHeader>
+    )
+}
---
+++ src/components/styles/Header.styled.js
@@ -0,0 +1,8 @@
+import styledComponents from "styled-components";
+
+export const StyledHeader = styledComponents.header`
+    background-color: ${({ bg }) => bg};
+    padding: 40px 0; 
+
+
+`
