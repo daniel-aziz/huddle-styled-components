37823268203e4420f768665b50a98ae86b8411f5
daniel-aziz <91468647+daniel-aziz@users.noreply.github.com>
2022-04-12 16:52:53 +0300

    finished landing page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./components/styles/Global";
+import content from './consts/content';
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 const theme = {
   colors: {
     header: '#ebfbff',
     body: '#fff',
     footer: '#003333'
-  }
+  },
+  mobile: "768px"
 }
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <>
+      
         <GlobalStyles />
-          <Header />
-          <Container className="App">
-            <h1>Hello World!</h1>
-          </Container>
+        <Header />
+        <Container className="App">
+          {content.map((item, index) => (
+            <Card key={index} item={item}></Card>
+          ))}
+        </Container>
+        <Footer />
+
       </>
     </ThemeProvider>
   );
--- src/components/styles/Container.styled.js
+++ src/components/styles/Container.styled.js
@@ -6,6 +6,4 @@ export const Container = styled.div`
     max-width: 100%;
     padding: 0 20px;
     margin: 0 auto;
-    color: red;
-    
 `
